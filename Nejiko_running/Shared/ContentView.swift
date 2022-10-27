f3468f6182fa8d4a50a9bdf191fd30138feb6e14
takaharayota <takahara-yota0710@g.ecc.u-tokyo.ac.jp>
2022-10-27 10:27:00 +0900

    nejiko

---
+++ Nejiko_running/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 高原陽太 on 2021/09/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nejiko_running/Shared/Nejiko_runningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nejiko_runningApp.swift
+//  Shared
+//
+//  Created by 高原陽太 on 2021/09/10.
+//
+
+import SwiftUI
+
+@main
+struct Nejiko_runningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
